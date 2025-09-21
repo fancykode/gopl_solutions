325d85604ef5d328c6a9b2a266224568b7a4be45
fancykode <james_j439@protonmail.com>
2025-09-21 20:06:30 +0200

    Exercise 3.13

---
+++ ch3/3_13/main.go
@@ -0,0 +1,24 @@
+/* Exercise 3.13: Write const declarations for KB, MB, up
+   through YB as compactly as you can.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	KB = 1000
+	MB = KB * KB
+	GB = MB * KB
+	TB = GB * KB
+	PB = TB * KB
+	EB = PB * KB
+	ZB = EB * KB
+	YB = ZB * KB
+)
+
+func main() {
+	fmt.Println(KB, MB, GB, TB, PB, EB)
+}
